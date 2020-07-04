488d3e3ae2cdcd3f2b380070062f1400d2552376
David Kern <david33belle@gmail.com>
2020-07-04 16:48:28 +0300

    Update index.js

--- src/library/unsplash/index.js
+++ src/library/unsplash/index.js
@@ -5,7 +5,7 @@ export const unsplashLib = _ => {
     var instance = null;
 
     if(!instance){
-        instance = new Unsplash({ accessKey: 'd1e939f23ed64823c2f7106759fa15a9b2414794b88cb59e68859b70f13ec6e5' });
+        instance = new Unsplash({ accessKey: 'key' });
     }
 
     return instance;
